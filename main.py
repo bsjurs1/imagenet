b650a44d35ec5904ee3feef2661251fe23faf1fa
bsjurs1 <b.sjursen@gmail.com>
2017-05-28 19:46:40 -0700

    Use VGGnet architecture

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ from ImageNet import ImageNet
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                    help='SGD momentum (default: 0.5)')
+parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for testing (default: 64)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 100)')
+parser.add_argument('--lr', type=float, default=0.5, metavar='LR',
+                    help='learning rate (default: 0.5)')
+parser.add_argument('--momentum', type=float, default=0.05, metavar='M',
+                    help='SGD momentum (default: 0.05)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -49,6 +49,7 @@ def imshow(img):
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 transform = transforms.Compose([
+    transforms.Scale(256),
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3081,))
     ])
@@ -74,70 +75,95 @@ class Net(nn.Module):
     def __init__(self):
         """Initialize the CNN."""
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv3 = nn.Conv2d(20, 30, kernel_size=3)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(2430, 200)
-        self.fc2 = nn.Linear(200, 100)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3)
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3)
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3)
+        self.conv7 = nn.Conv2d(256, 256, kernel_size=3)
+        self.conv8 = nn.Conv2d(256, 256, kernel_size=3)
+        self.conv9 = nn.Conv2d(256, 512, kernel_size=3)
+        self.conv10 = nn.Conv2d(512, 512, kernel_size=3)
+        self.conv11 = nn.Conv2d(512, 512, kernel_size=3)
+        self.conv12 = nn.Conv2d(512, 512, kernel_size=3)
+        self.conv13 = nn.Conv2d(512, 512, kernel_size=3)
+        self.conv14 = nn.Conv2d(512, 512, kernel_size=3)
+        self.conv15 = nn.Conv2d(512, 512, kernel_size=3)
+        self.conv16 = nn.Conv2d(512, 512, kernel_size=3)
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 100)
+
+        # self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        # self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.conv3 = nn.Conv2d(20, 30, kernel_size=3)
+        # self.conv2_drop = nn.Dropout2d()
+        # self.fc1 = nn.Linear(2430, 200)
+        # self.fc2 = nn.Linear(200, 100)
 
     def forward(self, x):
         """Perform the classification."""
-        # print("init size is: ")
-        # print(x.size())
 
         x = self.conv1(x)
-        # print("Size after conv1 is: ")
-        # print(x.size())
-
-        x = F.max_pool2d(x, 2)
-        # print("Size after avg_pool is: ")
-        # print(x.size())
-
         x = F.relu(x)
-        # print("Size after relu 1 is: ")
-        # print(x.size())
-
         x = self.conv2(x)
-        # print("Size after conv2 is: ")
-        # print(x.size())
+        x = F.relu(x)
 
         x = F.max_pool2d(x, 2)
-        # print("Size after max_pool2d is: ")
-        # print(x.size())
-
-        x = F.relu(x)
-        # print("Size 6 is: ")
-        # print(x.size())
 
         x = self.conv3(x)
-        # print("size after conv3 is: ")
-        # print(x.size())
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
 
-        x = x.view(-1, 2430)
-        # print("size after view is: ")
-        # print(x.size())
+        x = F.max_pool2d(x, 2)
+
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = self.conv6(x)
+        x = F.relu(x)
+        x = self.conv7(x)
+        x = F.relu(x)
+        x = self.conv8(x)
+        x = F.relu(x)
+
+        x = F.max_pool2d(x, 2)
+
+        x = self.conv9(x)
+        x = F.relu(x)
+        x = self.conv10(x)
+        x = F.relu(x)
+        x = self.conv11(x)
+        x = F.relu(x)
+        x = self.conv12(x)
+        x = F.relu(x)
+
+        x = F.max_pool2d(x, 2)
+
+        x = self.conv13(x)
+        x = F.relu(x)
+        x = self.conv14(x)
+        x = F.relu(x)
+        x = self.conv15(x)
+        x = F.relu(x)
+        x = self.conv16(x)
+        x = F.relu(x)
+
+        x = x.view(-1, 512)
 
         x = self.fc1(x)
-        # print("size after fc1 is: ")
-        # print(x.size())
-
-        x = F.relu(x)
-        # print("size after relu is: ")
-        # print(x.size())
-        x = F.dropout(x, training=self.training)
-        # print("size after drop out is: ")
-        # print(x.size())
         x = self.fc2(x)
-        # print("size after fc2 is: ")
-        # print(x.size())
-        # return F.log_softmax(x)
+        x = self.fc3(x)
+
+        # x = F.dropout(x, training=self.training)
+
         return F.log_softmax(x), F.softmax(x)
 
 
-model = Net().cuda()
-# if args.cuda:
-#     model.cuda()
+model = Net()
+if args.cuda:
+    model.cuda()
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
@@ -151,6 +177,8 @@ def train(epoch):
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         output, probvec = model(data)
+        print(output.size())
+        print(probvec.size())
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
