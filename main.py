32de59d87c8b1276c9a7702ed80ccf7afc973e7a
bsjurs1 <b.sjursen@gmail.com>
2017-05-30 11:48:01 -0700

    u

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
                     help='input batch size for testing (default: 128)')
-parser.add_argument('--epochs', type=int, default=128, metavar='N',
-                    help='number of epochs to train (default: 128)')
+parser.add_argument('--epochs', type=int, default=20, metavar='N',
+                    help='number of epochs to train (default: 20)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
