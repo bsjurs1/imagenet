6df8fb6f575bbdac42306520758782577c8fc1b4
bsjurs1 <b.sjursen@gmail.com>
2017-05-30 22:49:09 -0700

    u

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ class Net(nn.Module):
         self.conv6 = nn.Conv2d(256, 256, kernel_size=3)
         self.conv7 = nn.Conv2d(256, 512, kernel_size=3)
         self.fc1 = nn.Linear(512, 512)
-        self.fc2 = nn.Linear(512, 100)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 100)
 
     def forward(self, x):
         """Perform the classification."""
@@ -107,6 +108,7 @@ class Net(nn.Module):
 
         x = self.fc1(x)
         x = self.fc2(x)
+        x = self.fc3(x)
 
         # x = F.relu(self.conv1(x)) # 54
         # x = F.dropout(x, training=self.training)
